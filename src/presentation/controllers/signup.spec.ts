5f8d5ecb86e80097ea096389b51443dd40926881
Paulo Santos <paulosantosphs@hotmail.com>
2021-03-23 11:16:47 -0300

    refactor: use mockImplementationOnce instead of create EmailValidatorStup again

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -112,15 +112,8 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 500 if EmailValidator throws an Exception', () => {
-    // const { sut, emailValidatorStub } = makeSut()
-    // jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
-    class EmailValidatorStub implements EmailValidator {
-      isValid (email: string): boolean {
-        throw new Error()
-      }
-    }
-    const emailValidatorStub = new EmailValidatorStub()
-    const sut = new SignUpController(emailValidatorStub)
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => { throw new Error() })
     const httpRequest = {
       body: {
         name: 'any_name',
