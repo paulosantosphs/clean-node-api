ff25bb6a019fb3442b972695e4bfc5b1b9359076
Paulo Santos <paulosantosphs@hotmail.com>
2021-04-20 11:32:02 -0300

    refactor: chnage models to use type alias intead of interfaces

--- src/validation/validators/validation-composite.spec.ts
+++ src/validation/validators/validation-composite.spec.ts
@@ -2,7 +2,7 @@ import { MissingParamError } from '@/presentation/errors'
 import { ValidationComposite } from './validation-composite'
 import { Validation } from '@/presentation/protocols'
 
-interface SutTypes {
+type SutTypes = {
   sut: ValidationComposite
   validationStubs: Validation[]
 }
